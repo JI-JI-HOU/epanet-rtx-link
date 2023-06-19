fbae51a301c8f725f7ef8fec2f22fd7c70305383
JI-JI-HOU <115870994+JI-JI-HOU@users.noreply.github.com>
2023-06-19 14:40:10 +0800

    Update typeData.js

--- frontend/model/typeData.js
+++ frontend/model/typeData.js
@@ -26,7 +26,7 @@ const sourceTypes = {
         inputType:'text-line',
         key:'connectionString',
         text:'Connection',
-        placeholder:'Server=192.168.0.1;Port=1433;TDS_Version=7.1;Database=Historian_DB;UID=rtx;PWD=rtx',
+        placeholder:'Server=127.0.0.1;Port=3306;TDS_Version=7.1;Database=Historian_DB;UID=root;PWD=123456',
         helptext: 'ODBC Connection String. Enter the host, port, database, uid, pwd, and any driver-specific options'
       },{
         inputType: 'text-line',
